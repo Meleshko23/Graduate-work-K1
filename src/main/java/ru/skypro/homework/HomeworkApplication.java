3ced3609fec3749581c8a9e2d21f7250ddebea77
abdullinru <abdullin17021989@gmail.com>
2023-02-13 21:12:10 +0600

    добавил аннотацию OpenAPIDefinition

--- src/main/java/ru/skypro/homework/HomeworkApplication.java
+++ src/main/java/ru/skypro/homework/HomeworkApplication.java
@@ -1,10 +1,12 @@
 package ru.skypro.homework;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class HomeworkApplication {
 
     public static void main(String[] args) {
